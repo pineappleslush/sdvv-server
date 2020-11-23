d49245c0c43d832fb60718f25e7eae4166aae914
lani <lanimorton356@gmail.com>
2020-11-22 22:52:03 -0800

    Initial project created

---
+++ src/main/java/com/pineappleslush/sdvvserver/SdvvServerApplication.java
@@ -0,0 +1,13 @@
+package com.pineappleslush.sdvvserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdvvServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SdvvServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pineappleslush/sdvvserver/SdvvServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pineappleslush.sdvvserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdvvServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
